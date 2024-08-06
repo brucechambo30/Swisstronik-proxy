6d4a507cf34afb6aaa84b4cd1fbd5cd629b52477
brucechambo30 CCCCC <brucechambo1@gmail.com>
2024-08-06 15:46:50 +0000

    initial commit

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x4e47FC0F9B0Bd302f8C7c390D36205cADaf30AD5'
+export const ProxyAdmin = '0x193700e538b8652323818094876636De1FA32fb4'
+export const SWTRImplementation = '0xb18C2b4D50acce2beE75543DCeef2b647d69ac27'
+    
